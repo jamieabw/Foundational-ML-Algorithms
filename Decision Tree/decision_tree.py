7dc85110e25df7dfccadbcf0ec44884a4015f59a
Jamie Whitehair <jamieabw@protonmail.com>
2026-08-22 13:10:00 +0100

    More base decision tree implementations

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,25 +1,63 @@
+import numpy as np
+
 class Decision_Tree:
     def __init__(self):
         self.root = None
 
-    def train(self, data):
-        self.root = self.build_tree(data)
+    def train(self, data, labels):
+        self.root = self.build_tree(data, labels)
 
     def build_tree(self, data):
-        ...
+        best_score = float("inf")
+        best_feature = None
+        best_threshold = None
+        features = len(data[0]) # number of features to classify
+        for i in range(features):
+            ...
+        
 
-    def calculate_impurity(self, data):
+    def calculate_impurity(self, y):
         true_counter, false_counter = (0,0)
-        for d in data:
-            if d[-1] == 0:
+        for d in y:
+            if d == 0:
                 false_counter += 1
                 continue
             true_counter += 1
-        return 1 - ((true_counter/(true_counter + false_counter)) * (false_counter / (true_counter + false_counter)))
+        return 1 - (((true_counter/(true_counter + false_counter)) ** 2) +  ((false_counter / (true_counter + false_counter)) ** 2))
 
-    def split_data(self, data, feature):
-        # finds the best split.
+    def get_best_split(self, data):
         ...
+
+    def split_data(self, data, feature, threshold):
+        x, y = data
+        left_mask = x[:, feature] < threshold
+        right_mask = x[: feature] >= threshold
+        x_left = x[left_mask]
+        y_left = y[left_mask]
+        x_right = x[right_mask]
+        y_right = y[right_mask]
+        return x_left, y_left, x_right, y_right
+
+    def get_split_score(self, y_left, y_right):
+        n = len(y_left) + len(y_right)
+        left_weight = len(y_left) / n
+        right_weight = len(y_right) / n 
+        return (left_weight * self.calculate_impurity(y_left)) + (right_weight * self.calculate_impurity(y_right))    
+
 
     def predict(self, data):
         ...
+
+
+    def get_mid_points(self, data: list):
+        mid_points = []
+        for i in range(len(data) -1):
+            mid_points.append((data[i] + data[i+1]) / 2)
+        return mid_points
+
+
+
+training_data = [([5,8], 1),
+                  ([1,2], 0)]
+
+tree = Decision_Tree()
--- Decision Tree/node.py
+++ Decision Tree/node.py
@@ -1,9 +1,17 @@
 class Node:
-    def __init__(self, feature, threshold):
+    def __init__(self, feature=None, threshold=None, prediction=None, numerical=None, left=None, right=None):
         self.feature = feature
         self.threshold = threshold
+        self.numerical = numerical
 
-        self.left = None
-        self.right = None
-        self.prediction = None
+        self.left = left
+        self.right = right
+        self.prediction = prediction
+
+    def compare(self, data):
+        if self.numerical:
+            return data < self.threshold
+        else:
+            return data == self.threshold
+
         
