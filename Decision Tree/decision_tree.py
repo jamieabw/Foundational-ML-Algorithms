4c64208c86d282eeed7c01b4c77ef4edb9b1c044
Jamie Whitehair <jamieabw@protonmail.com>
2026-08-23 14:09:57 +0100

    Decision Tree implemented

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,19 +1,29 @@
 import numpy as np
+from node import Node
 
 class Decision_Tree:
     def __init__(self):
         self.root = None
 
-    def train(self, data, labels):
+    def train(self, data, labels, max_depth):
+        self.max_depth = max_depth
+        data = np.array(data)
+        labels = np.array(labels)
         self.root = self.build_tree(data, labels)
 
-    def build_tree(self, data):
-        best_score = float("inf")
-        best_feature = None
-        best_threshold = None
-        features = len(data[0]) # number of features to classify
-        for i in range(features):
-            ...
+    def build_tree(self, data, labels, depth=0):
+        if len(np.unique(labels)) == 1: # first stopping condition
+            return Node(prediction=labels[0])
+        if depth >= self.max_depth:
+            return Node(prediction=self.get_most_common_class(labels))
+        # second stopping condition needs to be max depth when implemented
+        feature, threshold = self.get_best_split(data, labels)
+        if feature is None:
+            return Node(prediction=self.get_most_common_class(labels))
+        x_left, y_left, x_right, y_right = self.split_data(data, labels, feature, threshold)
+        left = self.build_tree(x_left, y_left, depth=depth+1)
+        right = self.build_tree(x_right, y_right, depth=depth + 1)
+        return Node(feature=feature, threshold=threshold, left=left, right=right)
         
 
     def calculate_impurity(self, y):
@@ -25,17 +35,35 @@ class Decision_Tree:
             true_counter += 1
         return 1 - (((true_counter/(true_counter + false_counter)) ** 2) +  ((false_counter / (true_counter + false_counter)) ** 2))
 
-    def get_best_split(self, data):
-        ...
+    def get_best_split(self, data, labels):
+        best_feature = None
+        best_threshold = None
+        best_score = float("inf")
+        features = len(data[0]) # use first piece of data as a reference for number of features
+        for feature in range(features):
+            temp = []
+            for d in data:
+                temp.append(d[feature]) #
+            mid_points = self.get_mid_points(temp)
+            for threshold in mid_points:
+                x_left, y_left, x_right, y_right = self.split_data(data, labels, feature, threshold)
+                if len(y_left) == 0 or len(y_right) == 0:
+                    continue
+                
+                score = self.get_split_score(y_left, y_right)
+                if score < best_score:
+                    best_score = score
+                    best_threshold = threshold
+                    best_feature = feature
+        return best_feature, best_threshold
 
-    def split_data(self, data, feature, threshold):
-        x, y = data
-        left_mask = x[:, feature] < threshold
-        right_mask = x[: feature] >= threshold
-        x_left = x[left_mask]
-        y_left = y[left_mask]
-        x_right = x[right_mask]
-        y_right = y[right_mask]
+    def split_data(self, data, labels, feature, threshold):
+        left_mask = data[:, feature] < threshold
+        right_mask = data[:, feature] >= threshold
+        x_left = data[left_mask]
+        y_left = labels[left_mask]
+        x_right = data[right_mask]
+        y_right = labels[right_mask]
         return x_left, y_left, x_right, y_right
 
     def get_split_score(self, y_left, y_right):
@@ -46,18 +74,25 @@ class Decision_Tree:
 
 
     def predict(self, data):
-        ...
+        current_node = self.root
+        while current_node.prediction is None:
+            feature = current_node.feature
+            threshold = current_node.threshold
+            if data[feature] < threshold:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right 
+        return current_node.prediction
 
 
     def get_mid_points(self, data: list):
+        data = sorted(set(data))
         mid_points = []
         for i in range(len(data) -1):
             mid_points.append((data[i] + data[i+1]) / 2)
         return mid_points
 
+    def get_most_common_class(self, labels):
+        classes, counts = np.unique(labels, return_counts=True)
+        return classes[np.argmax(counts)]
 
-
-training_data = [([5,8], 1),
-                  ([1,2], 0)]
-
-tree = Decision_Tree()
---
+++ Decision Tree/main.py
@@ -0,0 +1,44 @@
+from decision_tree import Decision_Tree
+def main():
+    decision_tree = Decision_Tree()
+    training_data = [
+    [1, 5],
+    [2, 6],
+    [2, 8],
+    [3, 5],
+    [4, 7],
+    [5, 6],
+    [6, 8],
+    [7, 7],
+    [8, 9],
+    [9, 8],
+    [10, 7],
+    [11, 9],
+    [3,9],
+    [4,10],
+    [2,10],
+                ] # represents hours studied, hours slept
+    training_labels = [
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+                    ] # represents fail/pass test
+    decision_tree.train(training_data, training_labels, 5)
+    test_case = [5,8] # should pass
+    print(decision_tree.predict(test_case)) # outputs 1 correctly
+
+
+if __name__ == "__main__":
+    main()
